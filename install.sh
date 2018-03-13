aced605c0c6cedf97b0c0d69fcef916f5dff2906
Ben Mills <bmills@adobe.com>
2018-03-13 15:31:22 -0600

    Use pip3

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@
 #
 # Installs powerline-status via pip
 
-PIP_REQUIRE_VIRTUALENV="" pip show powerline-status > /dev/null
+PIP_REQUIRE_VIRTUALENV="" pip3 show powerline-status > /dev/null
 if [ $? -eq 1 ];
 then
   echo "  Installing powerline-status"
-  PIP_REQUIRE_VIRTUALENV="" pip install powerline-status
+  PIP_REQUIRE_VIRTUALENV="" pip3 install powerline-status
 else
   echo " powerline-status is installed"
 fi
