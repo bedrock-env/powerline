63c3941beb206b0e997fc680e9130bb7d2e489cf
Ben Mills <ben@unfiniti.com>
2018-10-10 14:39:04 -0600

    Improve pathing

--- config.zsh
+++ config.zsh
@@ -1,6 +1,4 @@
 # powerline
-export PYTHONPATH="$HOME/Development/bedrock/extensions/powerline/:${PYTHONPATH}"
 export POWERLINE_FULL_CURRENT_PATH="true"
 export POWERLINE_SHOW_GIT_ON_RIGHT="true"
 export POWERLINE_DETECT_SSH="true"
-export POWERLINE_ROOT=/usr/local/lib/python3.6/site-packages/powerline
--- init.zsh
+++ init.zsh
@@ -1,1 +1,3 @@
+POWERLINE_ROOT="$($PYTHON_BASE_DIR/bin/pip3 show powerline-status | grep Location | cut -d ' ' -f 2)/powerline"
+
 source $POWERLINE_ROOT/bindings/zsh/powerline.zsh
